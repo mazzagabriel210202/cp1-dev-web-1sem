90d18aceb00540762c179abc70a742681b528d45
gabriel <gabrielmazza2102@gmail.com>
2023-04-28 18:22:07 -0300

    trabalho checkpoint

--- script1.js
+++ script1.js
@@ -1,7 +1,33 @@
-let texto = prompt("vamos conhecer sobre vinhos")
-console.log(texto)
 
-let titulo = document.querySelector(h1)
+alert("vamos conhecer sobre vinhos")
+
+var vinho = prompt("tinto ou branco")
+
+if(vinho.toLowerCase() == "tinto") {
+   alert("voce escolheu tinto")
+
+}else{
+  alert("voce escolheu branco")  
+}
+
+var vinho2 = prompt("forte ou fraco")
+
+if(vinho2.toLowerCase() == "forte") {
+   alert("voce escolheu forte")
+
+}else{
+  alert("voce escolheu fraco")  
+}
+
+
+var vinho3 = prompt("nacional ou importado")
+
+if(vinho3.toLowerCase() == "nacional") {
+   alert("voce escolheu nacional")
+
+}else{
+  alert("voce escolheu importado")  
+}
 
 
 
